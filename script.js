8d5a8575f6e413e61f2961122b23b8e74aea9bed
BHUSHN1611 <bhushanjadhao1603@gmail.com>
2024-08-05 21:52:52 +0530

    border color change

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function printDetails() {
     #main{
         height:600px;
         width: 390px;
-        border: 1px solid rgb(0, 0, 0);
+        border: 1px solid rgb(255, 255, 255);
         margin-left:auto ;
         margin-right: auto;
         position: relative;
